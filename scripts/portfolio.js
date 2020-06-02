aa58c7446c96919cdd7abb5581bce50ca2a20c5b
1McFly10 <vlados3702@gmail.com>
2020-06-02 21:42:50 +0300

    FIX SHOW MORE

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -3,10 +3,43 @@ let portfolio_card = document.querySelector('.portfolio-card-wrapper');
 let slider_images_element = document.querySelectorAll('.portfolio-card-slider-image');
 let slider_count_element = document.querySelectorAll('.slider-count');
 let show_more_button = document.querySelector('.remont-types-button-portfolio');
+
+let project_name_array = ['Вторичка1', 'Новострой2','Вторичка3', 'Новострой4','Вторичка5', 'Новострой6','Вторичка7', 'Новострой8','Вторичк9', 'Новострой10','Вторичка11', 'Новострой12'];
+let project_name = document.querySelectorAll('.project-name');
+let project_area_array = [183, 65, 98, 76, 62, 75, 111, 29, 50, 80, 43, 39];
+let project_area = document.querySelectorAll('.project-area');
+
+let images_for_card_array = ['"images/галлерея/1.png"', '"images/галлерея/2.png"', '"images/галлерея/3.png"']
+let images_for_card =  document.querySelectorAll('#portfolio-card-slider-main-image');
+// FOR PORTFOLIO CARDS
+for (let x = 0; x < project_name.length; x++) {
+    for (let i = 0; i < project_name_array.length; i++) {
+        project_name[x].innerHTML = project_name_array[x];
+    }
+}
+for (let x = 0; x < project_area.length; x++) {
+    for (let i = 0; i < project_area_array.length; i++) {
+        project_area[x].innerHTML = project_area_array[x] + ' м²';
+    }
+}
+
+// FOR PORTFOLIO CARD!
+console.log(images_for_card)
+
+
+
+
+
+
+
+
+
 for (let i = 0; i < button_info.length; i++){
     button_info[i].addEventListener('click',()=>{
         portfolio_card.style.display ='flex';
     })
+
+
 }
 
 $('.portfolio-card-slider-images img').click(function(e){
--- scripts/show_more.js
+++ scripts/show_more.js
@@ -1,10 +1,21 @@
 
 let show_more_buttons = document.querySelectorAll('.show-more');
 let show_more_text = document.querySelectorAll('.show-more-text');
+// let
 for( let i =0; i<show_more_buttons.length; i++){
     show_more_buttons[i].addEventListener('click',(e)=>{
         console.log(e.target);
         show_more_buttons[i].style.display = 'none';
+
+        if(i==0){
+          document.querySelector('.remont-types-block-wrapper-one').style.height = '750px';
+        }
+        else if(i==1){
+            document.querySelector('.remont-types-block-wrapper-two').style.height = '750px'
+        }
+        else if(i==2){
+            document.querySelector('.remont-types-block-wrapper-three').style.height = '750px'
+        }
 
         for (let y = 0; y < show_more_text.length; y++) {
             show_more_text[i].style.display = 'flex';
