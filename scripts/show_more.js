8a0a82ac66c7991ad5ef74fcd19a22942611331d
1McFly10 <vlados3702@gmail.com>
2020-06-03 19:20:34 +0300

    fix remont types height blocks

--- scripts/show_more.js
+++ scripts/show_more.js
@@ -8,13 +8,13 @@ for( let i =0; i<show_more_buttons.length; i++){
         show_more_buttons[i].style.display = 'none';
 
         if(i==0){
-          document.querySelector('.remont-types-block-wrapper-one').style.height = '750px';
+          document.querySelector('.remont-types-block-wrapper-one').style.height = '800px';
         }
         else if(i==1){
-            document.querySelector('.remont-types-block-wrapper-two').style.height = '750px'
+            document.querySelector('.remont-types-block-wrapper-two').style.height = '1100px'
         }
         else if(i==2){
-            document.querySelector('.remont-types-block-wrapper-three').style.height = '750px'
+            document.querySelector('.remont-types-block-wrapper-three').style.height = '970px'
         }
 
         for (let y = 0; y < show_more_text.length; y++) {
