bec916ad8c4182f63fb4ad79fd4109a4eb8ee722
1McFly10 <vlados3702@gmail.com>
2020-06-04 11:16:41 +0300

    fixed portfolio-card-info button

--- scripts/show_more.js
+++ scripts/show_more.js
@@ -6,15 +6,26 @@ for( let i =0; i<show_more_buttons.length; i++){
     show_more_buttons[i].addEventListener('click',(e)=>{
         console.log(e.target);
         show_more_buttons[i].style.display = 'none';
-
-        if(i==0){
-          document.querySelector('.remont-types-block-wrapper-one').style.height = '800px';
+        console.log(window.innerWidth);
+        if(window.innerWidth <=1024 || window.innerWidth < 768){
+            console.log('DONE');
+            if (i == 0) {
+                document.querySelector('.remont-types-block-wrapper-one').style.height = '980px';
+            } else if (i == 1) {
+                document.querySelector('.remont-types-block-wrapper-two').style.height = '1430px'
+            } else if (i == 2) {
+                document.querySelector('.remont-types-block-wrapper-three').style.height = '1200px'
+            }
         }
-        else if(i==1){
-            document.querySelector('.remont-types-block-wrapper-two').style.height = '1100px'
-        }
-        else if(i==2){
-            document.querySelector('.remont-types-block-wrapper-three').style.height = '970px'
+        else {
+            console.log('ERROR');
+            if (i == 0) {
+                document.querySelector('.remont-types-block-wrapper-one').style.height = '800px';
+            } else if (i == 1) {
+                document.querySelector('.remont-types-block-wrapper-two').style.height = '1100px'
+            } else if (i == 2) {
+                document.querySelector('.remont-types-block-wrapper-three').style.height = '970px'
+            }
         }
 
         for (let y = 0; y < show_more_text.length; y++) {
@@ -22,3 +33,5 @@ for( let i =0; i<show_more_buttons.length; i++){
         }
     })
 }
+
+
